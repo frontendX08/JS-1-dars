edb62d94932549f244e8c6f2ba507a79e9a53084
frontendX08 <abdullamamatraimov19@gmail.com>
2023-12-24 20:20:38 +0500

    add the JS 1-dars

---
+++ javascript/java.js
@@ -0,0 +1,18 @@
+var name = prompt ('Ismingizni kiriting');
+var surname = prompt ('Familyangizni kiriting');
+var age = prompt ('Yoshingizni kiriting');
+var moon = prompt ('Tug\'ulgan oyingizni kiriting');
+var email = prompt ('Emailingizni kiriting');
+var tel = prompt ('Telefon raqamingizni kiriting');
+var state = prompt ('Qaysi davlatda yashaysiz');
+var ipAdress = prompt ('Hozirda qayerda istiqomat qilasiz');
+var address = prompt ('Mazilingizni kiriting');
+var interest = prompt ('Qiziqishingizni kiriting');
+var occupation = prompt ('Kasbingizni kiriting');
+var course = prompt ('Siz berilgan kurslarni tugatdingizmi');
+var programming = prompt ('Siz dasturlashni o\'rgandingizmi');
+var theWork = prompt ('Bizga ishga kirmoqchimisiz???');
+var index = prompt ('Aperato\'rlarimiz bilan bog\'lanib dacumentlarni to\'ldiring');
+var final = prompt ('Barcha so\'rovlarni to\'ldirdingizmi?');
+
+console.log(typeof `Sizning malumotlaringiz bizga to\'g\'i keldi ishga olindingiz\n 1.ism:${name} \n 2.familya:${surname} \n 3.yosh:${age} \n 4.moon:${moon} \n 5.email:${email}\n 6.tel:${tel}\n 7.state:${state}\n 8.ipAdress:${ipAdress}\n 9.address:${address}\n 10.interest:${interest}\n 11.occupation:${occupation}\n 12.course:${course}\n 13.programming:${programming }\n 14.theWork:${theWork}`);
